4b1e3680a9a2a4796618599b222fd671db901956
Luis Fraguada <luis@mcneel.com>
2020-07-29 10:46:12 +0200

    updating dependencies

--- src/App.vue
+++ src/App.vue
@@ -19,25 +19,24 @@ export default {
   },
   methods: {
     makeThings () {
-
-      if(window.loaded){
-        let m = new Module.Sphere([0,0,0], 10);
-        console.log(m);
+      if (window.loaded) {
+        const m = new Module.Sphere([0, 0, 0], 10)
+        console.log(m)
       }
-      
-      let num = 100;
-      for(let i = 0; i < num; i++) {
-          let x = Math.random()*(20 - -20) + -20;
-          let y = Math.random()*(20 - -20) + -20;
-          let z = Math.random()*(20 - -20) + -20;
 
-          var geometry = new THREE.SphereBufferGeometry( 0.5, 10, 10 );
-          var material = new THREE.MeshBasicMaterial( {color: 0xff0000, wireframe:true} );
-          var sphere = new THREE.Mesh( geometry, material );
-          sphere.position.x = x;
-          sphere.position.y = y;
-          sphere.position.z = z;
-          scene.add( sphere );
+      const num = 100
+      for (let i = 0; i < num; i++) {
+        const x = Math.random() * (20 - -20) + -20
+        const y = Math.random() * (20 - -20) + -20
+        const z = Math.random() * (20 - -20) + -20
+
+        var geometry = new THREE.SphereBufferGeometry(0.5, 10, 10)
+        var material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+        var sphere = new THREE.Mesh(geometry, material)
+        sphere.position.x = x
+        sphere.position.y = y
+        sphere.position.z = z
+        scene.add(sphere)
       }
     }
   }
